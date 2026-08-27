380d69355021b8a383f4bb09b70946e7eb9c61fa
annajostes2018-debug <annajostes2018@gmail.com>
2026-08-27 17:36:25 -0500

    Update

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-document.querySelector('.btn__menu').addEventListener('click', function() {
-    const menu = document.querySelector('.menu');
-    if (menu.style.visibility === 'hidden' || menu.style.visibility === '') {
-        menu.style.visibility = 'visible';
-        menu.style.opacity = '1';
-        menu.style.transform = 'translateX(0)';
-    } else {
-        menu.style.visibility = 'hidden';
-        menu.style.opacity = '0';
-        menu.style.transform = 'translateX(300%)';
-    }
-});
-document.querySelector('.btn__menu').addEventListener('click', function() {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('is-visible'); // Toggle a class
-});
+function openMenu() {
+    document.querySelector(".menu").classList.add("menu--open");
+}
+
+function closeMenu() {
+    document.querySelector(".menu").classList.remove("menu--open");
+}
